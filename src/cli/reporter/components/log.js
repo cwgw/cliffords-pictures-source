e8e565a9e0612a6fa12b21702d5fb8c90ce4e12a
Charlie Wright <charlie@cwgw.co>
2019-12-11 11:22:19 -0800

    Add Color component

--- src/cli/reporter/components/log.js
+++ src/cli/reporter/components/log.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Box, Color} from 'ink';
+import {Box} from 'ink';
+
+import Color from './color';
 
 const propTypes = {
 	children: PropTypes.oneOfType([
@@ -20,24 +22,12 @@ const defaultProps = {
 	strong: false,
 };
 
-const statusColor = {
-	success: 'green',
-	warning: 'yellow',
-	log: 'gray',
-	info: 'cyan',
-	error: 'red',
-	panic: 'red',
-};
-
 const Log = ({timestamp, children, color, prefix, strong}) => {
-	const colorProps = {inverse: strong};
-	if (color) {
-		colorProps[statusColor[color] || color] = true;
-	}
-
 	return (
 		<Box textWrap="wrap" flexDirection="row" alignItems="flex-start">
-			<Color {...colorProps}>{[timestamp, '░', prefix].join(' ').trim()}</Color>
+			<Color color={color} inverse={strong}>
+				{[timestamp, '░', prefix].join(' ').trim()}
+			</Color>
 			<Box marginX={1}>{children}</Box>
 		</Box>
 	);
